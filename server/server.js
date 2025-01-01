4d373b97ea972f5601b15e0af2400e4798afc5b7
Aaditya Patil <aadityapatil081@gmail.com>
2025-01-01 18:19:50 +0530

    c

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: `${process.env.CLIENT_URL}`,
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
